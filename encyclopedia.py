2257508e8b0a92bfa45b9599262e83a249f2be51
ghvill <ghvilela@users.noreply.github.com>
2021-04-12 19:47:53 -0300

    added q&a list and dict + bug fixes

--- encyclopedia.py
+++ encyclopedia.py
@@ -2,6 +2,7 @@ import random
 from cfonts import render
 import getpass
 
+
 def create_num_list(n1,n2):
     num_list = []
     for i in range(n1,n2+1):
@@ -12,6 +13,143 @@ def create_num_list(n1,n2):
 
 easy_mode = {}
 hard_mode = {}
+
+random_quotes = [
+"""Today is the tomorrow we worried about yesterday"""
+,
+"""It's easier to see the mistakes on someone else's paper"""
+,
+"""Every man dies. Not every man really lives"""
+,
+"""The day is already blessed, find peace within it"""
+,
+"""From small beginnings come great things"""
+,
+"""Learning is a treasure that will follow its owner everywhere"""
+,
+"""Talk doesn't cook rice"""
+,
+"""When deeds speak, words are nothing"""
+,
+"""Some pursue happiness, others create it"""
+,
+"""It takes both sunshine and rain to make a rainbow"""
+,
+"""A beautiful thing is never perfect"""
+,
+"""If you are patient in one moment of anger, you will escape one hundred days of sorrow"""
+,
+"""A good rest is half the work"""
+,
+"""Every new day is another chance to change your life"""
+]
+
+
+easy_list_raw = [{
+"quoteText": """Genius is one percent inspiration and ninety-nine percent perspiration.""",
+"quoteAuthor": "Thomas Edison"
+}, {
+"quoteText": """A house divided against itself cannot stand.""",
+"quoteAuthor": "Abraham Lincoln"
+}, {
+"quoteText": """Difficulties increase the nearer we get to the goal.""",
+"quoteAuthor": "Johann Wolfgang von Goethe"
+}, {
+"quoteText": """Peace comes from within. Do not seek it without.""",
+"quoteAuthor": "Buddha"
+}, {
+"quoteText": """To lead people walk behind them.""",
+"quoteAuthor": "Lao Tzu"
+}, {
+"quoteText": """Having nothing, nothing can he lose.""",
+"quoteAuthor": "William Shakespeare"
+}, {
+"quoteText": """Everything in life is luck.""",
+"quoteAuthor": "Donald Trump"
+}, {
+"quoteText": """Study the past, if you would divine the future.""",
+"quoteAuthor": "Confucius"
+}, {
+"quoteText": """God always takes the simplest way.""",
+"quoteAuthor": "Albert Einstein"
+}, {
+"quoteText": """Be as you wish to seem.""",
+"quoteAuthor": "Socrates"
+}, {
+"quoteText": """Who sows virtue reaps honour.""",
+"quoteAuthor": "Leonardo da Vinci"
+}, {
+"quoteText": """Be kind whenever possible. It is always possible.""",
+"quoteAuthor": "Dalai Lama"
+}, {
+"quoteText": """Do, or do not. There is no try.""",
+"quoteAuthor": "Yoda"
+}, {
+"quoteText": """Courage is going from failure to failure without losing enthusiasm.""",
+"quoteAuthor": "Winston Churchill"
+}, {
+"quoteText": """Luck is what happens when preparation meets opportunity.""",
+"quoteAuthor": "Seneca"
+}, {
+"quoteText": """Victory belongs to the most persevering.""",
+"quoteAuthor": "Napoleon Bonaparte"
+}]
+
+hard_list_raw = [{
+"quoteText": """Time is the wisest counsellor of all.""",
+"quoteAuthor": "Pericles"
+}, {
+"quoteText": """The two most powerful warriors are patience and time.""",
+"quoteAuthor": "Leo Tolstoy"
+}, {
+"quoteText": """No man was ever wise by chance.""",
+"quoteAuthor": "Seneca"
+}, {
+"quoteText": """Reason and free inquiry are the only effectual agents against error.""",
+"quoteAuthor": "Thomas Jefferson"
+}, {
+"quoteText": """If you want a thing done well, do it yourself.""",
+"quoteAuthor": "Napoleon Bonaparte"
+}, {
+"quoteText": """Wrinkles should merely indicate where smiles have been.""",
+"quoteAuthor": "Mark Twain"
+}, {
+"quoteText": """Mediocrity knows nothing higher than itself, but talent instantly recognizes genius.""",
+"quoteAuthor": "Arthur Conan Doyle"
+}, {
+"quoteText": """Don't leave a stone unturned. It's always something, to know you have done the most you could.""",
+"quoteAuthor": "Charles Dickens"
+}, {
+"quoteText": """A short saying often contains much wisdom.""",
+"quoteAuthor": "Sophocles"
+}, {
+"quoteText": """I don't believe in failure. It is not failure if you enjoyed the process.""",
+"quoteAuthor": "Oprah Winfrey"
+}, {
+"quoteText": """Living at risk is jumping off the cliff and building your wings on the way down.""",
+"quoteAuthor": "Ray Bradbury"
+}, {
+"quoteText": """It all depends on how we look at things, and not how they are in themselves.""",
+"quoteAuthor": "Carl Jung"
+}, {
+"quoteText": """The greatest minds are capable of the greatest vices as well as of the greatest virtues.""",
+"quoteAuthor": "Rene Descartes"
+}, {
+"quoteText": """And as we let our own light shine, we unconsciously give other people permission to do the same""",
+"quoteAuthor": "Nelson Mandela"
+}]
+
+
+
+for item in easy_list_raw:
+    for text in item.values():
+        if not text in (easy_mode.values() or easy_mode.keys()):
+            easy_mode[item["quoteAuthor"]] = item["quoteText"]
+
+for item in hard_list_raw:
+    for text in item.values():
+        if not text in (hard_mode.values() or hard_mode.keys()):
+            hard_mode[item["quoteAuthor"]] = item["quoteText"]
 
 ## title screen
 
@@ -120,11 +258,10 @@ print(render(start_message))
 for round in range(1,rounds+1):
     ##4.1 Question asking
     round_question = questions.pop(random.randint(0,len(questions)-1))
-    round_answers = question_list[round_question]
-    correct_answer = question_list[round_question][0]
+    correct_answer = question_list[str(round_question)]
     round_message = """
     Question {n}:
-    {q}""".format(n=round, q=round_question)
+    Come up a nice "{q}" quote: """.format(n=round, q=round_question.upper())
     print(round_message)
     
     ##4.2 display player input options and build pool
@@ -141,10 +278,10 @@ for round in range(1,rounds+1):
         else:
             player_scores[player] += 10
     while len(answer_pool) < 5:
-        random_answer = round_answers[random.randint(1,len(round_answers)-1)]
-        if not random_answer in answer_pool:
+        random_answer = random_quotes[random.randint(1,len(random_quotes)-1)]
+        if not random_answer.upper() in answer_pool:
             answer_pool.append(random_answer.upper())
-    print("Now choose an answer from the pool:")
+    print("Each of these from the pool did he acctualy say?")
     pool_display = {}
     option = 0
     for answer in random.sample(answer_pool, len(answer_pool)):
@@ -154,7 +291,7 @@ for round in range(1,rounds+1):
     ## Choose answer and score points
     correct_choice = ""
     for opt, answer in pool_display.items():
-        if answer == correct_answer:
+        if answer == correct_answer.upper():
             correct_choice = opt
     for player in players_list:
         player_choice = input("{player}: ".format(player=player))
